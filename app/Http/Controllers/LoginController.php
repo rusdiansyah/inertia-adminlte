9f29ee4a8dbc5acdee8643ea10a77211637af2b3
Deddy Rusdiansyah <deddy.rusdiansyah@gmail.com>
2025-09-12 17:49:59 +0700

    route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,26 +11,14 @@ class LoginController extends Controller
 {
     public function register(Request $request)
     {
-        // Validate
         $fields = $request->validate([
-            // 'avatar' => ['file', 'nullable', 'max:3000'],
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email','lowercase', 'max:255', 'unique:users'],
             'password' => ['required','min:3', 'confirmed']
         ]);
-
-        // Save avatar if it exists
-        // if ($request->hasFile('avatar')) {
-        //     $fields['avatar'] = Storage::disk('public')->put('avatars', $request->avatar);
-        // }
         $fields['isActive'] = 1;
-        // Register
         $user = User::create($fields);
-
-        // Login
         Auth::login($user);
-
-        // Redirect
         return redirect()
             ->route('dashboard')
             ->with('message', 'Welcome to Laravel Inertia Vue app');
@@ -38,15 +26,13 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        // sleep(2);
         $fields = $request->validate([
             'email' => ['required', 'email', 'lowercase'],
             'password' => ['required'],
         ]);
         if (Auth::attempt($fields, $request->remember)) {
             $request->session()->regenerate();
-            return redirect()->intended('dashboard');
-            // return redirect()->route('dashboard');
+            return Inertia::location(route('dashboard'));
         }
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
